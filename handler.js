b7dab3f146c6e9738686f75f963dd67dbbe8860d
disha9848 <68861635+disha9848@users.noreply.github.com>
2021-05-05 13:21:35 +0530

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,68 @@
+'use strict';
+const AWS= require('aws-sdk');
+const db= new AWS.DynamoDB.DocumentClient()
+const uuid=require('uuid/v4');
+
+const postsTable= process.env.POSTS_TABLE;
+
+function response(statusCode, message){
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify(message)
+  };
+}
+
+//const formatResponse = function(statusCode, body) {
+//  const response = {
+//      statusCode: statusCode,
+//      headers: {
+//          "Access-Control-Allow-Origin": "*",
+//          "Content-Type": "application/json"
+//      },
+//      isBase64Encoded: false,
+//      body: JSON.stringify(body)
+//  };
+//  return response;
+// };
+
+
+//module.exports.createPost =async (event, callback) => {
+//  const reqBody=  JSON.parse(event.body)
+//  const post ={
+//    id: uuid(),
+//    userId: 1,
+//    title: reqBody.title,
+//    body: reqBody.body
+//  };
+//  const response= await addPost(post);
+//  return formatResponse(200,response)
+
+// };
+
+// async function addPost(post){
+//  db.put({
+//    TableName: postsTable,
+//    Item: post
+//  })
+//  return ('post is inserted');
+// }
+
+module.exports.createPost= async (event, context, callback) => {
+  const reqBody= JSON.parse(event.body)
+  console.log(reqBody)
+
+  const post ={
+    id: uuid(),
+    userId: 1,
+    title: reqBody.title,
+    body: reqBody.body
+  };
+
+  return db.put({
+    TableName: postsTable,
+    Item: post
+  }).promise().then(() => {
+    callback(null, response(201, post))
+  })
+  .catch(err => response(null,response(err.statusCode, err)));
+}
